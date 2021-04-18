f12dc45cd591b43e9e74308dfbbdffc4e58cd9e9
Alice Oliveira <aoqb@cin.ufpe.br>
2021-04-17 21:36:11 -0300

    adicionando consulta

---
+++ ProjetoFisico/consultas.sql
@@ -0,0 +1,73 @@
+-- INNER JOIN projeta o nome e a data de nascimento de todos os médicos
+select nome, dtNascimento 
+from treinador p
+inner join medico m
+on p.cpf = m.cpf;
+
+select a.cidade, a.cod, a.cor, a.cpf
+from insignia a
+inner join treinador b
+ON (a.cpf = b.cpf);
+
+select a.especie, a.car_personalidade, a.cpf
+from pokewinx a
+inner join treinador b
+ON (a.cpf = b.cpf);
+
+select a.nome, a.cpf
+from treinador a
+inner join medico b
+ON (a.cpf = b.cpf);
+
+-- OUTER JOIN
+
+select a.especie, a.car_personalidade, a.cpf
+from pokewinx a
+right outer join treinador b
+on (a.cpf = b.cpf);
+
+select a.especie, a.car_personalidade, a.cpf
+from pokewinx a
+left outer join treinador b
+on (a.cpf = b.cpf);
+
+select a.especie, a.car_personalidade, a.cpf
+from pokewinx a
+full outer join treinador b
+on (a.cpf = b.cpf)
+order by especie;
+
+-- ANTI JUNCAO projeta a espécie e o id de todos os pokemons que não são tratados por nenhum médico
+select p.especie, p.idt
+from pokewinx p
+where not exists (select * from trata t where t.idt = p.idt);
+
+-- SUBCONSULTA ESCALAR
+
+select nome
+from treinador where 
+dtNascimento > any (select dtNascimento from treinador);
+
+select nome
+from treinador
+where dtNascimento < any (select dtNascimento from treinador);
+
+select especie
+from pokewinx
+where car_altura > (select avg(car_altura) from pokewinx);
+
+select cod
+from batalha
+where data_bat > any (select data_bat from batalha);
+
+-- SUBCONSULTA LINHA
+-- projeta a espécie dos pokemons com o mesmo peso e altura do pokemon de id 19
+select especie
+from pokewinx
+where (car_peso, car_altura) = (select car_peso, car_altura from pokewinx where idt = 19);
+
+-- projeta a espécie e o id dos pokemons maiores que o pokemon de id 19
+select especie, idt
+from pokewinx, (select car_altura as throh_altura, car_peso as throh_peso from pokewinx where idt = 19)
+where throh_altura < car_altura and throh_peso < car_peso;
+
---
+++ ProjetoFisico/plsql.sql
@@ -0,0 +1,64 @@
+-- ALICEEEEEEEEEEE
+
+create or replace procedure pokemon_tipo (idPoke number) is 
+    cursor cur_poke is 
+        select tipo 
+        from tipos 
+        where idt = idPoke; 
+    tipoPoke tipos.tipo%TYPE; 
+begin 
+    open cur_poke; 
+    loop 
+        fetch cur_poke into tipoPoke; 
+        exit when cur_poke%notfound; 
+        dbms_output.put_line('tipo: '||tipoPoke); 
+    end loop; 
+    close cur_poke; 
+end;
+
+create or replace procedure pokemon_esp_tipo (esp varchar) is  
+    cursor cur_poke is  
+        select tipo  
+        from tipos t inner join pokewinx p on t.idt = p.idt 
+        where especie = esp;  
+    tipoPoke tipos.tipo%TYPE;  
+begin  
+    open cur_poke;  
+    loop  
+        fetch cur_poke into tipoPoke;  
+        exit when cur_poke%notfound;  
+        dbms_output.put_line('tipo: '||tipoPoke);  
+    end loop;  
+    close cur_poke;  
+end;
+
+create or replace trigger trigger_change_insignia 
+after update of cpf on insignia 
+referencing old as o new as n 
+for each row 
+begin 
+    if (:o.cpf is null) then 
+        dbms_output.put_line('Atribuindo insignia ao treinador: '||:n.cpf); 
+    end if; 
+    if (:o.cpf is not null) then 
+        dbms_output.put_line('Atribuindo insignia ao treinador: '||:n.cpf||' Antigo treinador: '||:o.cpf); 
+    end if; 
+end;
+
+create or replace procedure nome_procedimento is 
+    pesoAux number(6,2):= 5.0; 
+    cursor cur_peso is 
+        select idt, especie 
+        from pokewinx 
+        where car_peso > pesoAux; 
+    idPoke pokewinx.idt%TYPE; 
+    esp pokewinx.especie%TYPE; 
+begin 
+    open cur_peso; 
+    loop 
+        fetch cur_peso into idPoke, esp; 
+        exit when cur_peso%NOTFOUND; 
+        DBMS_OUTPUT.PUT_LINE(idPoke || ' ' || esp); 
+    end loop; 
+    close cur_peso; 
+end;
