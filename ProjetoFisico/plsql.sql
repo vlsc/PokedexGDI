bf0e57477585194ef83eabe58261f389667c5d86
aliceoq <62757720+aliceoq@users.noreply.github.com>
2021-04-21 18:31:37 -0300

    atualizandoa parte de pl/sql

--- ProjetoFisico/plsql.sql
+++ ProjetoFisico/plsql.sql
@@ -1,113 +1,77 @@
--- ALICEEEEEEEEEEE
-
-create or replace procedure pokemon_tipo (idPoke number) is 
-    cursor cur_poke is 
-        select tipo 
-        from tipos 
-        where idt = idPoke; 
-    tipoPoke tipos.tipo%TYPE; 
-begin 
-    open cur_poke; 
-    loop 
-        fetch cur_poke into tipoPoke; 
-        exit when cur_poke%notfound; 
-        dbms_output.put_line('tipo: '||tipoPoke); 
-    end loop; 
-    close cur_poke; 
-end;
-
-create or replace procedure pokemon_esp_tipo (esp varchar) is  
-    cursor cur_poke is  
-        select tipo  
-        from tipos t inner join pokewinx p on t.idt = p.idt 
-        where especie = esp;  
-    tipoPoke tipos.tipo%TYPE;  
-begin  
-    open cur_poke;  
-    loop  
-        fetch cur_poke into tipoPoke;  
-        exit when cur_poke%notfound;  
-        dbms_output.put_line('tipo: '||tipoPoke);  
-    end loop;  
-    close cur_poke;  
-end;
-
-create or replace trigger trigger_change_insignia 
-after update of cpf on insignia 
-referencing old as o new as n 
-for each row 
-begin 
-    if (:o.cpf is null) then 
-        dbms_output.put_line('Atribuindo insignia ao treinador: '||:n.cpf); 
-    end if; 
-    if (:o.cpf is not null) then 
-        dbms_output.put_line('Atribuindo insignia ao treinador: '||:n.cpf||' Antigo treinador: '||:o.cpf); 
-    end if; 
-end;
-
-create or replace procedure nome_procedimento is 
-    pesoAux number(6,2):= 5.0; 
-    cursor cur_peso is 
-        select idt, especie 
-        from pokewinx 
-        where car_peso > pesoAux; 
-    idPoke pokewinx.idt%TYPE; 
-    esp pokewinx.especie%TYPE; 
-begin 
-    open cur_peso; 
-    loop 
-        fetch cur_peso into idPoke, esp; 
-        exit when cur_peso%NOTFOUND; 
-        DBMS_OUTPUT.PUT_LINE(idPoke || ' ' || esp); 
-    end loop; 
-    close cur_peso; 
-end;
-
---Allan
-
---Function
-
-CREATE OR REPLACE FUNCTION findPokewinx (especiePokeWinx VARCHAR)  RETURN VARCHAR IS
-
-    CURSOR cursor_Pokewinx IS
-    SELECT *
-    FROM pokewinx;
-    
-    BEGIN
-        
-        FOR reg_Pokewinx IN cursor_Pokewinx LOOP
-        
-            IF(reg_Pokewinx.especie = especiePokeWinx) THEN
-                return 'TRUE';
-            END IF;
-            
-        END LOOP;
-        
-        return 'FALSE';
-        
-        EXCEPTION 
-            WHEN NO_DATA_FOUND THEN 
-                dbms_output.put_line('Dados indisponiveis da tabela');
-                RETURN NULL;
-        
-    END;
-    
--- Matheus
-
+-- O GATILHO É ATIVADO APÓS A INSERÇÃO EM BATALHA E ATUALIZA A INSÍGNIA QUE FOI REFERENCIADA EM BATALHA, PARA QUE ESSA PERTENÇA AO TREINADOR REFERENCIADO NA MESMA BATALHA  
 create or replace trigger trigger_ganha_batalha
 after insert on batalha
 referencing new as n
 for each row
-when(n.cidade is not null and n.cod is not null)
+when (n.cidade is not null and n.cod is not null)
 begin
-  update insignia
-  set cpf = :n.cpf_treinador
-  where cidade = :n.cidade and cod = :n.cod;
+    update insignia
+    set cpf = :n.cpf_treinador
+    where cidade = :n.cidade and cod = :n.cod;
 end;
 
+
+-- O GATILHO É ATIVADO APÓS ATRIBUIR UM TREINADOR PARA UM POKEWINX
+create or replace trigger trigger_treina_pokewinx
+after insert cpf on pokewinx
+for each row
+begin
+dbms_output.put_line('Atribuindo pokewinx ao treinador: '||:n.cpf); 
+end;
+
+-- O GATILHO É ATIVADO APÓS A ATUALIZAÇÃO DE CPF EM UMA INSÍGNIA. IMPRIME UMA STRING INFORMANDO O NOVO CPF E O ANTIGO (SE HOUVER).
+create or replace trigger trigger_change_insignia
+after update of cpf on insignia
+referencing old as o new as n
+for each row
+begin
+    if (:o.cpf is null) then 
+        dbms_output.put_line('Atribuindo insignia ao treinador: '||:n.cpf); 
+    else
+        dbms_output.put_line('Atribuindo insignia ao treinador: '||:n.cpf
+   ||'Antigo treinador: '||:o.cpf); 
+    end if; 
+end;
+
+-- IMPRIME O(S) TIPO(S) DE UM POKEMON A PARTIR DO SEU ID.
+create or replace procedure pokemon_tipo (idPoke number) is
+    cursor cur_poke is
+        select tipo
+        from tipos
+        where idt = idPoke;
+    tipoPoke tipos.tipo%TYPE;
+begin
+    open cur_poke;
+    loop
+            fetch cur_poke into tipoPoke;
+            exit when cur_poke%notfound;
+            dbms_output.put_line('tipo: '||tipoPoke);
+    end loop;
+    close cur_poke;
+end;
+
+-- ATUALIZA INSIGNIA A PARTIR DOS VALORES DE CPF, CIDADE E CÓDIGO FORNECIDOS COMO PAR METROS NA PROCEDURE (FEITO PRA FORNECER UM CONTADOR COMO PAR METRO EM CÓDIGO)
 create or replace procedure adiciona_insignia_sem_batalha(cp varchar, cid varchar, cd number) is
 begin
   update insignia
   set cpf = cp
   where cidade = cid and cod = cd;
+end;
+
+-- FUNÇÃO RETORNA UMA STRING PARA INFORMAR SE AQUELA ESPÉCIE DE POKEWINX JÁ EXISTE NO BANCO DE DADOS.
+create or replace function findPokewinx (especiePokeWinx varchar) return varchar is
+cursor cursor_Pokewinx is
+select *
+from pokewinx;
+begin
+for reg_Pokewinx in cursor_Pokewinx loop
+if (reg_Pokewinx.especie = especiePokeWinx) then return 'TRUE';
+end if;
+end loop;
+return 'FALSE';
+        
+        exception
+            when NO_DATA_FOUND then
+                dbms_output.put_line('Dados indisponiveis da tabela');
+                 return null;
 end;
